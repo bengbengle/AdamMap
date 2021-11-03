05e807a21ad0256d3aebc49a46039cfde7178361
bengbengle <bengbengle@gmail.com>
2021-11-03 19:15:17 +0800

    first commit

---
+++ src/bundles/peers.js
@@ -0,0 +1,139 @@
+import { createAsyncResourceBundle, createSelector } from 'redux-bundler'
+import ms from 'milliseconds'
+import multiaddr from 'multiaddr'
+
+const swarmPeersTTL = ms.seconds(10)
+const bundle = createAsyncResourceBundle({
+  name: 'peers',
+  actionBaseType: 'PEERS',
+  getPromise: async ({ getIpfs }) => {
+    window.global_ipfs = getIpfs()
+
+    // return getIpfs().swarm.peers({ verbose: true, timeout: '10000ms' })
+    //   .then(res => {
+    //     console.log('getipfs...', res)
+    //   })
+
+    const req = await fetch('https://scan.adamoracle.io/api/getMachineSubStep', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        currentPage: '1',
+        pageSize: '100000'
+      })
+    })
+    return req.json().then(data => {
+      window.global_list = data.data.records
+
+      const list = window.global_list.map((m, idx) => {
+        const addr = '/ip4/' + m.ip + '/tcp/80'
+        return {
+          power: m.diskSize,
+          poolname: m.poolName || '-',
+          direction: 2,
+          latency: '0ms',
+          muxer: '',
+          addr: multiaddr(addr),
+          peer: m.machineCode
+        }
+      })
+      return list
+    })
+  },
+  staleAfter: swarmPeersTTL,
+  persist: false,
+  checkIfOnline: false
+})
+
+const asyncResourceReducer = bundle.reducer
+
+bundle.reducer = (state, action) => {
+  const asyncResult = asyncResourceReducer(state, action)
+
+  if (action.type === 'SET_SELECTED_PEER') {
+    return { ...asyncResult, selectedPeers: action.payload }
+  }
+
+  return asyncResult
+}
+
+bundle.selectPeersCount = createSelector(
+  'selectPeers',
+  (peers) => {
+    if (!Array.isArray(peers)) return 0
+    return peers.length
+  }
+)
+
+bundle.doConnectSwarm = (addr, permanent) => async ({ dispatch, getIpfs, store }) => {
+  dispatch({ type: 'SWARM_CONNECT_STARTED', payload: { addr } })
+  const ipfs = getIpfs()
+  try {
+    await ipfs.swarm.connect(addr)
+
+    if (permanent) {
+      const maddr = multiaddr(addr)
+      const peerId = maddr.getPeerId()
+      const rawAddr = maddr.decapsulateCode(421).toString() // drop /p2p suffix
+
+      // TODO: switch to ipfs.swarm.peering when https://github.com/ipfs/go-ipfs/pull/8147 ships
+      let peers = (await ipfs.config.get('Peering.Peers')) || []
+      const preexisting = peers.find(p => p.ID === peerId)
+      if (preexisting) {
+        if (!preexisting.Addrs.find(a => a === rawAddr)) {
+          // add new addr to existing address list for the peer
+          preexisting.Addrs.push(rawAddr)
+        }
+      } else {
+        // add new peer to the list
+        peers = [...peers, { ID: peerId, Addrs: [rawAddr] }]
+      }
+
+      await ipfs.config.set('Peering.Peers', peers)
+      await store.doMarkConfigAsOutdated() // force Settings screen to re-fetch
+    }
+  } catch (err) {
+    return dispatch({
+      type: 'SWARM_CONNECT_FAILED',
+      payload: { addr, error: err }
+    })
+  }
+
+  return dispatch({ type: 'SWARM_CONNECT_FINISHED', payload: { addr } })
+}
+
+// Update the peers if they are stale (appTime - lastSuccess > staleAfter)
+bundle.reactPeersFetchWhenIdle = createSelector(
+  'selectPeersShouldUpdate',
+  'selectIpfsConnected',
+  (shouldUpdate, ipfsConnected) => {
+    if (shouldUpdate && ipfsConnected) {
+      return { actionCreator: 'doFetchPeers' }
+    }
+  }
+)
+
+// Get the peers frequently when we're on the peers page
+bundle.reactPeersFetchWhenActive = createSelector(
+  'selectAppTime',
+  'selectRouteInfo',
+  'selectPeersRaw',
+  'selectIpfsConnected',
+  (appTime, routeInfo, peersInfo, selectIpfsReady, ipfsConnected) => {
+    const lastSuccess = peersInfo.lastSuccess || 0
+    // if (routeInfo.url === '/peers' && ipfsConnected && !peersInfo.isLoading && appTime - lastSuccess > ms.seconds(5)) {
+    if (ipfsConnected && !peersInfo.isLoading && appTime - lastSuccess > ms.seconds(5)) {
+      return { actionCreator: 'doFetchPeers' }
+    }
+  }
+)
+
+bundle.selectSelectedPeers = (state) => state.peers.selectedPeers
+
+bundle.doSetSelectedPeers = (peer) => ({ dispatch }) => {
+  dispatch({ type: 'SET_SELECTED_PEER', payload: peer })
+}
+
+export default bundle
---
+++ src/peers/WorldMap/WorldMap.js
@@ -0,0 +1,223 @@
+import React, { useCallback, useState, useEffect } from 'react'
+import ReactFauxDOM from 'react-faux-dom'
+import { connect } from 'redux-bundler-react'
+import { withTranslation } from 'react-i18next'
+import * as d3 from 'd3'
+// import CountryFlag from 'react-country-flag'
+import { debounce } from 'redux-bundler'
+
+import staticMapSrc from './StaticMap.svg'
+
+import Address from '../../components/address/Address'
+import Popover from '../../components/popover/Popover'
+
+// Styles
+import './WorldMap.css'
+// import Cid from '../../components/cid/Cid'
+
+const calculateWidth = (windowWidth) => {
+  // the d3 generated svg width includes a lot of ocean, that we crop for now, as it looks weird.
+  const svgWidthOversizeFactor = 1.7
+  // remove a constant amount for the chrome that surronds the map.
+  const sidebarAndPadding = 350
+  const availableWidth = windowWidth - sidebarAndPadding
+  const width = availableWidth * svgWidthOversizeFactor
+  // if the map gets too big the dots get lost in the dot grid, also it just overloads the viewers brain.
+  if (width > 3000) {
+    return 3000
+  }
+  // if the map gets too small it becomes illegible. There will be some map cropping on mobile.
+  if (width < 700) {
+    return 700
+  }
+
+  return width
+}
+
+const calculateHeight = (width) => {
+  if (width > 960) {
+    if (window.innerHeight < 940) {
+      return (window.innerHeight - 180) * 0.6
+    }
+    return width * 0.273
+  }
+
+  return width * 0.5
+}
+
+const WorldMap = ({ t, className, selectedPeers, doSetSelectedPeers }) => {
+  const [width, setWidth] = useState(calculateWidth(window.innerWidth))
+  const [height, setHeight] = useState(calculateHeight(width))
+  const [selectedTimeout, setSelectedTimeout] = useState(null)
+
+  useEffect(() => {
+    const debouncedHandleResize = debounce(() => {
+      const width = calculateWidth(window.innerWidth)
+      setWidth(width)
+      setHeight(calculateHeight(width))
+    }, 100)
+
+    window.addEventListener('resize', debouncedHandleResize)
+
+    return () => window.removeEventListener('resize', debouncedHandleResize)
+  })
+
+  const handleMapPinMouseEnter = useCallback((peerIds, element) => {
+    if (!element) return
+
+    clearTimeout(selectedTimeout)
+
+    const { x, y, width, height } = element.getBBox()
+
+    doSetSelectedPeers({ peerIds, left: `${x + width / 2}px`, top: `${y - height / 2}px` })
+  }, [doSetSelectedPeers, selectedTimeout])
+
+  const handleMapPinMouseLeave = useCallback(() => {
+    setSelectedTimeout(
+      setTimeout(() => doSetSelectedPeers({}), 6000)
+    )
+  }, [doSetSelectedPeers])
+
+  const handlePopoverMouseEnter = useCallback(() => clearTimeout(selectedTimeout), [selectedTimeout])
+
+  return (
+    <div className="flex flex-column">
+      <div className={`relative ${className}`}>
+        <div className='mb1 flex flex-column items-center'>
+          <div className="relative no-events" style={{ width, height, background: `transparent url(${staticMapSrc}) center no-repeat`, backgroundSize: 'auto 100%' }}>
+            <GeoPath width={width} height={height}>
+              { ({ path }) => (
+                <MapPins width={width} height={height} path={path} handleMouseEnter={ handleMapPinMouseEnter } handleMouseLeave= { handleMapPinMouseLeave } />
+              )}
+            </GeoPath>
+            { selectedPeers?.peerIds && (
+              <Popover show={ !!(selectedPeers.top && selectedPeers.left) }
+                top={ selectedPeers.top }
+                left={ selectedPeers.left }
+                align='bottom'
+                handleMouseEnter={ handlePopoverMouseEnter }
+                handleMouseLeave={ handleMapPinMouseLeave }>
+                <PeerInfo ids={ selectedPeers.peerIds } t={t}/>
+              </Popover>)
+            }
+          </div>
+        </div>
+        <div className='no-events absolute bottom-0 left-0 right-0'>
+          <div className='flex flex-auto flex-column items-center self-end pb5-ns no-select'>
+            <div className='f1 fw5 black'><PeersCount /></div>
+            <div className='f4 b ttu charcoal-muted'>{'Machines'}</div>
+          </div>
+        </div>
+      </div>
+      <div className='relative flex justify-end pt2 pb4'>
+        <div className='f6 p2 no-select flex items-center'>
+          <span className='f6 charcoal-muted pr3'>{'Machines'}: </span>
+          <i className='mapDotExplanation mr1' style={{ width: getDotsSize(1) * 2, height: getDotsSize(1) * 2, backgroundColor: getDotsColor(1) }}></i>1-5
+          <i className='mapDotExplanation ml3 mr1' style={{ width: getDotsSize(5) * 2, height: getDotsSize(5) * 2, backgroundColor: getDotsColor(5) }}></i> 5-10
+          <i className='mapDotExplanation ml3 mr1' style={{ width: getDotsSize(11) * 2, height: getDotsSize(11) * 2, backgroundColor: getDotsColor(11) }}></i>11+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const PeersCount = connect('selectPeers', ({ peers }) => peers ? peers.length : 0)
+
+const GeoPath = ({ width, height, children }) => {
+  // https://github.com/d3/d3-geo/blob/master/README.md#geoEquirectangular
+  const projection = d3.geoEquirectangular()
+    .scale(height / Math.PI)
+    .translate([width / 2, height / 2])
+    .precision(0.1)
+  // https://github.com/d3/d3-geo/blob/master/README.md#paths
+  const path = d3.geoPath().projection(projection)
+
+  return children({ path })
+}
+
+const getDotsSize = (numberOfDots) => {
+  if (numberOfDots < 5) return 5
+  if (numberOfDots < 10) return 8
+  return 8
+}
+
+const getDotsColor = (numberOfDots) => {
+  if (numberOfDots < 5) return 'rgba(255, 107, 34, 0.6)'
+  if (numberOfDots < 10) return 'rgba(255, 107, 34, 0.8)'
+  return 'rgba(255, 107, 34, 0.8)'
+}
+
+// Just the dots on the map, this gets called a lot.
+const MapPins = connect('selectPeersCoordinates', ({ width, height, path, peersCoordinates, handleMouseEnter, handleMouseLeave }) => {
+  const el = d3.select(ReactFauxDOM.createElement('svg'))
+    .attr('width', width)
+    .attr('height', height)
+    .attr('viewBox', `0 0 ${width} ${height}`)
+
+  peersCoordinates.forEach(({ peerIds, coordinates }) => {
+    // console.log(', coordinates:: ', coordinates, ',, peerIds::', peerIds, ',,peersCoordinates::', peersCoordinates)
+    el.append('path')
+      .datum({
+        type: 'Point',
+        coordinates: coordinates
+      })
+      .attr('d', path.pointRadius(() => getDotsSize(peerIds.length)))
+      .attr('fill', () => getDotsColor(peerIds.length))
+      .attr('class', 'mapDot')
+      .on('mouseenter', () => handleMouseEnter(peerIds, d3.event.relatedTarget))
+      .on('mouseleave', () => handleMouseLeave())
+  })
+
+  return el.node().toReact()
+})
+
+const MAX_PEERS = 5
+
+const PeerInfo = connect('selectPeerLocationsForSwarm', ({ ids, peerLocationsForSwarm: allPeers, t }) => {
+  if (!allPeers) return null
+
+  console.log('allPeers::', allPeers, ',,,ids::', ids)
+
+  const peers = allPeers.filter(({ peerId }) => ids.includes(peerId))
+
+  if (!peers.length) return null
+
+  // const isWindows = useMemo(() => window.navigator.appVersion.indexOf('Win') !== -1, [])
+  return (
+    <div className="f6 flex flex-column justify-center">
+      { peers.sort((a, b) => a.address.localeCompare(b.address)).map((peer, index) => {
+        if (index === MAX_PEERS && peers.length > MAX_PEERS) {
+          return (<div className="f7 pa1 self-end" key="worldmap-more-label">{t('plusPeers', { number: peers.length - MAX_PEERS })}</div>)
+        }
+
+        if (index > MAX_PEERS) return null
+
+        return (
+          <div className="pa2 f7 flex items-center monospace" key={peer.peerId}>
+            {/* <CountryFlag className='mr1' code={peer.flagCode} svg={isWindows}/> */}
+            <div className="flex flex-auto items-center">
+              <Address value={ peer.address }/>
+              &nbsp;
+              <span className="charcoal-muted">
+                {
+                  peer.peerId
+                }
+              </span>
+              &nbsp;
+              <span className="charcoal-muted">
+                {peer.poolname || '???'}
+              </span>
+            </div>
+            <span className="gray ml1"> ( { peer.power + 'T' })</span>
+          </div>
+        )
+      }) }
+    </div>
+  )
+})
+
+export default connect(
+  'selectSelectedPeers',
+  'doSetSelectedPeers',
+  withTranslation('peers')(WorldMap)
+)
